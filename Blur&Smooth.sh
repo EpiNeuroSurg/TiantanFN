9d81210d797e3d456541d8e8d48b2305bb13c46b
zhangkai62035 <54886685+zhangkai62035@users.noreply.github.com>
2019-09-04 21:25:22 +0800

    Update and rename CortiComplexity&Smooth.sh to Blur&Smooth.sh

--- Blur&Smooth.sh
+++ Blur&Smooth.sh
@@ -5,5 +5,6 @@ for h in $H
 do
 pctsurfcon --s "$s" --gm-proj-frac 0.5 --neg --b gradient
 mris_fwhm --s "$s" --hemi "$h" --cortex --smooth-only --fwhm 5 --i "$s"/surf/"$h".gradient.mgh --o "$s"/surf/"$h".gradient.sm5.mgh
+mri_segstats --in ${SUBJECTS_DIR}/$s/surf/"$h".gradient.sm5.mgh --annot $s "$h" aparc --sum ${SUBJECTS_DIR}/$s/stats/"$h".gradient.sm5.stats --snr
 done
 done
