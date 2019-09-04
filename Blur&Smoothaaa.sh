912c276e4574e71838e04633d7ab06daaadd4bfd
zhangkai62035 <54886685+zhangkai62035@users.noreply.github.com>
2019-09-04 21:27:40 +0800

    Delete Blur&Smoothaaa.sh

--- Blur&Smoothaaa.sh
+++
@@ -1,8 +0,0 @@
-for s in `cat bu.txt`
-do
-H="lh rh"
-for h in $H
-do
-mri_segstats --in ${SUBJECTS_DIR}/$s/surf/"$h".gradient.sm5.mgh --annot $s "$h" aparc --sum ${SUBJECTS_DIR}/$s/stats/"$h".gradient.sm5.stats --snr
-done
-done
